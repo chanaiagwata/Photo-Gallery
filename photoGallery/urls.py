d94bbffab2fcd88371671cbfd660da0b0d263e0e
chanaiagwata <chanai.agwata@student.moringaschool.com>
2022-05-28 22:35:48 +0300

    feat:will add categories

--- photoGallery/urls.py
+++ photoGallery/urls.py
@@ -4,5 +4,9 @@ from . import views
 urlpatterns = [
     url('^$', views.index,name = 'indexpage'),
     url('about', views.about, name='aboutpage'),
-    url('gallery', views.gallery, name='gallerypage')
+    url('gallery', views.gallery, name='gallerypage'),
+    url('category/art', views.art, name = 'artpage'),
+    url('category/food', views.food, name = 'foodpage'),
+    url('category/nature', views.nature, name = 'naturepage'),
+    url('category/sports', views.sports, name = 'sportspage')
 ]
--- photoGallery/views.py
+++ photoGallery/views.py
@@ -1,3 +1,4 @@
+from pydoc import render_doc
 from django.shortcuts import render
 
 # Create your views here.
@@ -8,3 +9,15 @@ def about(request):
     return render(request, 'about.html')
 def gallery(request):
     return render(request, 'gallery.html')
+
+def art(request):
+    return render(request, 'category/art.html')
+
+def food(request):
+    return render(request, 'category/food.html')
+
+def nature(request):
+    return render(request, 'category/nature.html')
+
+def sports(request):
+    return render(request, 'category/sports.html')
