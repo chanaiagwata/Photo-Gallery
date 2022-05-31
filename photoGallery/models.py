4fa59a334843695a9024fb2b8eb7d00393227676
chanaiagwata <chanai.agwata@student.moringaschool.com>
2022-06-01 00:03:36 +0300

    adds on delete

--- photoGallery/models.py
+++ photoGallery/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-import datetime as dt
+
 
 # Create your models here.
 class Category(models.Model):
@@ -39,12 +39,12 @@ class Location(models.Model):
 
 class Image(models.Model):
     name = models.CharField(max_length =80)
-    category = models.ForeignKey('Category')
+    location = models.ForeignKey('Location', on_delete=models.CASCADE,)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE,)
     description = models.TextField()
-    location = models.ForeignKey('Location')
     pub_date = models.DateTimeField(auto_now_add=True)
     photo = models.ImageField(upload_to = 'photos/')
-    
+
     
     def __str__(self):
         return self.name
